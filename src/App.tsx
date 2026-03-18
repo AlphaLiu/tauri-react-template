62cf404533f7a00e2b6444f362941b8b3a437082
Alpha Liu <alpha.liu@me.com>
2026-03-18 11:45:11 +0800

    feat: add example of tauri commands

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
+
+import { commands } from "@/bindings";
+import { useState } from "react";
+
 
 export function App() {
+  const [greetMsg, setGreetMsg] = useState("");
+
+  const greeting = async () => {
+    const msg = await commands.greet("World");
+    setGreetMsg(msg);
+  }
+
   return (
     <div className="flex min-h-svh p-6">
       <div className="flex max-w-md min-w-0 flex-col gap-4 text-sm leading-loose">
@@ -8,7 +19,10 @@ export function App() {
           <h1 className="font-medium">Project ready!</h1>
           <p>You may now add components and start building.</p>
           <p>We&apos;ve already added the button component for you.</p>
-          <Button className="mt-2">Button</Button>
+          {
+            greetMsg && <p>{greetMsg}</p>
+          }
+          <Button className="mt-2" onClick={greeting}>Button</Button>
         </div>
         <div className="font-mono text-xs text-muted-foreground">
           (Press <kbd>d</kbd> to toggle dark mode)
