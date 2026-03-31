d7cc2ab720ea75ae52e93778fcd418e87b79d39d
Alpha Liu <alpha.liu@me.com>
2026-03-31 10:46:19 +0800

    🐛 fix: lint fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,19 @@
-import { Button } from "@/components/ui/button";
+import { useState } from 'react';
 
-import { commands } from "@/bindings";
-import { useState } from "react";
-
+import { commands } from '@/bindings';
+import { Button } from '@/components/ui/button';
 
 export function App() {
-  const [greetMsg, setGreetMsg] = useState("");
+  const [greetMsg, setGreetMsg] = useState('');
 
   const greeting = async () => {
-    const msg = await commands.greet("World");
+    const msg = await commands.greet('World');
     setGreetMsg(msg);
-  }
+  };
 
   return (
     <div className="flex min-h-svh p-6">
-      <div className="flex max-w-md min-w-0 flex-col gap-4 text-sm leading-loose">
+      <div className="flex max-w-md min-w-0 flex-col gap-4 text-sm/loose">
         <div>
           <h1 className="font-medium">Project ready!</h1>
           <p>You may now add components and start building.</p>
@@ -25,11 +24,15 @@ export function App() {
           <Button className="mt-2" onClick={greeting}>Button</Button>
         </div>
         <div className="font-mono text-xs text-muted-foreground">
-          (Press <kbd>d</kbd> to toggle dark mode)
+          (Press
+          {' '}
+          <kbd>d</kbd>
+          {' '}
+          to toggle dark mode)
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
