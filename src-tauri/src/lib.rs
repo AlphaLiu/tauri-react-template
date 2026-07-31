17b27fb6c89081fd154a17f225c663e46fcff2f6
Alpha Liu <alpha.liu@me.com>
2026-07-31 14:30:11 +0800

    feat: add tauri-specta

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,7 +1,35 @@
+use specta_typescript::Typescript;
+
+use tauri::ipc::Channel;
+use tauri_specta::{collect_commands, collect_events, Builder};
+
+#[tauri::command]
+#[specta::specta]
+fn greet(name: String) -> String {
+  format!("Hello, {}!", name)
+}
+
+#[tauri::command]
+#[specta::specta]
+fn test_channel(on_event: Channel<String>) {
+  // channel test, send events back to frontend
+  // send a string back to frontend
+  // useless function, just for testing
+  on_event.send(format!("testing")).unwrap();
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+  let builder = Builder::<tauri::Wry>::new()
+    // Then register them (separated by a comma)
+    .commands(collect_commands![greet, test_channel,])
+    .events(collect_events![]);
+  #[cfg(debug_assertions)] // <- Only export on non-release builds
+  builder
+    .export(Typescript::default(), "../src/bindings.ts")
+    .expect("Failed to export typescript bindings");
   tauri::Builder::default()
-    .setup(|app| {
+    .setup(move |app| {
       if cfg!(debug_assertions) {
         app.handle().plugin(
           tauri_plugin_log::Builder::default()
@@ -9,6 +37,10 @@ pub fn run() {
             .build(),
         )?;
       }
+
+      // Mount events using the moved builder
+      builder.mount_events(app);
+
       Ok(())
     })
     .run(tauri::generate_context!())
